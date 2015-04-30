11f9bf1ba94bc40c693585f8684b2ce157123a02
Ian Bottomley <github@kyloe.net>
2015-04-30 19:39:18 +0100

    Added SH to CWP

--- CWP.pl
+++ CWP.pl
@@ -10,13 +10,14 @@ use Kyloe::CWP::Connector;
 my $calendars  = 
 	{
         'Laura Hendry'       =>  {staffid => '20108436',	password => '170882',			altsummary=>['CODE'], summary=>['DEP','-','ARR']},
+        'Sarah Hendry'       =>  {staffid => '20109916',	password => '291182',			altsummary=>['CODE'], summary=>['DEP','-','ARR']},
     };
     
 foreach $name (keys %$calendars)  
 	{
 
 	my $cwp = Kyloe::CWP::Connector->new();
-	
+	# 	print "Process ".$calendars->{$name}."\n";
 	$cwp->login($calendars->{$name}->{staffid},$calendars->{$name}->{password}) or die "Login failed\n";
 	$cwp->writeICS($calendars->{$name});
 	undef $cwp;
